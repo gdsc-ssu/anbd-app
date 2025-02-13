88439fd8ffd8556678bf73ee6784a94ef5f24703
Chaerrrish <dhcofls0899@naver.com>
2025-02-13 22:37:17 +0900

    [feat] onboarding_model 추가

---
+++ lib/models/onboarding_model.dart
@@ -0,0 +1,34 @@
+class OnboardingInfo {
+  OnboardingInfo({
+    required this.content,
+    required this.image});
+
+  final String content;
+  final String image;
+}
+
+class OnboardingItems {
+  static String basePath = "assets/images/onboarding";
+
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+        content: "무심코 버리려던 물건들이 해양과 환경에 악영향을 끼치고 있다는 사실을 아시나요?",
+        image: "$basePath/onboarding1.png"),
+
+    OnboardingInfo(
+        content: "버리는 대신, ANBD에 업로드해서 새 주인을 찾아줄 수 있어요",
+        image: "$basePath/onboarding2.png"),
+
+    OnboardingInfo(
+        content: "ANBD에서는 내가 가지고 있는 물건을 이웃에게 무료나눔 하고",
+        image: "$basePath/onboarding3.png"),
+
+    OnboardingInfo(
+        content: "나눔한 금액을 내 이름으로 환경을 위해 기부할 수 있어요",
+        image: "$basePath/onboarding4.png"),
+
+    OnboardingInfo(
+        content: "ANBD를 통해 이웃과 정을 나누고 환경을 지켜요",
+        image: "$basePath/onboarding5.png"),
+  ];
+}
