0d1bbbdf7eb06cf7c8f77ec0a283f7632630f7bc
Seoneui <ellieella2031@gmail.com>
2025-03-09 14:32:28 +0900

    fix: BasicButton 사이즈 추가 구현

--- lib/widgets/basic_button.dart
+++ lib/widgets/basic_button.dart
@@ -1,23 +1,33 @@
 import 'package:anbd/constants/constants.dart';
 import 'package:flutter/material.dart';
 
+enum BasicButtonSize {
+  SMALL,
+  LARGE,
+}
+
+const double _smallheight = 38;
+const double _largeheight = 50;
+
 class BasicButton extends StatelessWidget {
   final String text;
   final bool isClickable;
   final VoidCallback? onPressed;
+  final BasicButtonSize size;
 
   const BasicButton({
     super.key,
     required this.text,
     required this.isClickable,
     required this.onPressed,
+    this.size = BasicButtonSize.LARGE, //기본값 large
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: 50,
+      height: (size == BasicButtonSize.LARGE) ? _largeheight : _smallheight,
       child: ElevatedButton(
         onPressed: isClickable ? onPressed : null,
         style: ElevatedButton.styleFrom(
