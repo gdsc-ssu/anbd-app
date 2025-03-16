5668631137eeea90e3025fc7abffccf1a34db6a9
Seoneui Jee <146940671+sunnny619@users.noreply.github.com>
2025-03-16 17:04:26 +0900

    [Setting/#21] Dio를 이용한 서버 통신 세팅 (#22)
    * feat: SecureStorageRepository 구현
    
    * chore: gitignore 추가 세팅
    
    * feat: dio 통신 세팅

---
+++ lib/data/di/api_client.dart
@@ -0,0 +1,67 @@
+import 'dart:developer';
+
+import 'package:anbd/data/repository/local/secure_storage_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+class ApiClient {
+  static final ApiClient _instance = ApiClient._internal();
+  late Dio _dio;
+  final SecureStorageRepository _secureStorage = SecureStorageRepository();
+
+  factory ApiClient() {
+    return _instance;
+  }
+
+  ApiClient._internal() {
+    _dio = Dio(BaseOptions(
+      baseUrl: FlutterConfig.get("base_url"),
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 15),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ));
+
+    /// Token Interceptor 추가
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        if (options.extra['skipAuthToken'] != true) {
+          var token = await _secureStorage.readAccessToken();
+          log("secureStorage $token");
+          if (token != null) {
+            options.headers['Authorization'] = 'Bearer $token';
+          }
+        }
+        log('요청 보내는 중: ${options.method} ${options.path}');
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        /// 응답 처리
+        log('응답 : ${response.statusCode}');
+        return handler.next(response);
+      },
+      onError: (DioException e, handler) {
+        /// 에러 처리
+        log('Error: ${e.message}');
+        return handler.next(e);
+      },
+    ));
+
+    // Log Interceptor 추가
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+      error: true,
+      logPrint: (obj) {
+        log("서버통신 $obj");
+      },
+    ));
+  }
+
+  Dio get dio => _dio;
+}
---
+++ lib/data/repository/local/secure_storage_repository.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorageRepository {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  ///토큰
+  Future<String?> readAccessToken() async {
+    return await _storage.read(key: "access_token");
+  }
+
+  Future<void> saveAccessToken(String accessToken) async {
+    await _storage.write(key: "access_token", value: accessToken);
+  }
+
+  Future<String?> readRefreshToken() async {
+    return await _storage.read(key: "refresh_token");
+  }
+
+  Future<void> saveRefreshToken(String refreshToken) async {
+    await _storage.write(key: "refresh_token", value: refreshToken);
+  }
+}
